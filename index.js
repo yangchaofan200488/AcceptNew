6e4125e2711fb2b6db2ccf4ec1d7df8e9924521d
yangchaofan <3509714363@qq.com>
2024-09-24 17:00:07 +0800

    完成加载动画

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // 首页轮播图
-setTimeout(function auto(){
-    // 获取当前正在显示的图片
-    const show=document.querySelector(".img-list .show-img")
+setTimeout(function auto() {
+  // 获取当前正在显示的图片
+  const show = document.querySelector(".img-list .show-img");
 
-    // 获取当前正在显示的元素的下一个兄弟元素(获取下一个图片)如果已经是最后一个图片了，就获取第一个图片
-    const show_next=show.nextElementSibling||document.querySelector(".carousel .img-list li")
+  // 获取当前正在显示的元素的下一个兄弟元素(获取下一个图片)如果已经是最后一个图片了，就获取第一个图片
+  const show_next =
+    show.nextElementSibling || document.querySelector(".carousel .img-list li");
 
-    // 修改显示状态
-    show.classList.remove("show-img")
-    show_next.classList.add("show-img")
+  // 修改显示状态
+  show.classList.remove("show-img");
+  show_next.classList.add("show-img");
 
-    setTimeout(auto,3000)
-},3000)
+  setTimeout(auto, 3000);
+}, 3000);
+
+window.onload = function () {
+  if (!sessionStorage.getItem("flag")) {
+    // 如果没有加载过动画，播放动画
+    setTimeout(() => {
+      document.querySelector(".outter").style.display = "none";
+      document.querySelector(".home").style.display = "block";
+      // 设置标志，表示已经加载过
+      sessionStorage.setItem("flag", "true");
+    }, 3000); // 这里设置为3秒
+  } else {
+    // 直接显示主页面
+    document.querySelector(".outter").style.display = "none";
+    document.querySelector(".home").style.display = "block";
+  }
+};
