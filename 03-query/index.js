c860fe776413bf253c33c2c7c5347c7496e1faee
lujiaqin <2623128580@qq.com>
2024-09-28 16:56:16 +0800

    改了一点逻辑

--- 03-query/index.js
+++ 03-query/index.js
@@ -1,6 +1,6 @@
 // 返回警告未保存
 function warning(){
-  const isJudge = confirm('尚未提交申请，是否确认返回');
+  const isJudge = confirm('信息尚未保存，是否确定返回');
   if(isJudge) {
     history.go(-1)
   }
@@ -15,16 +15,16 @@ function toResult(){
   const phoneNumber = document.querySelector('.phoneNumber').value.replace(/\s/g, '')
   const phoneReg = /^1\d{10}$/
   const qqReg = /^[1-9][0-9]{4,14}$/
-  if(!relName || !speciality || !className || !qqNumber) {
-    alert('请将信息填写完整!!!')
-    return
-  }
-  if(!phoneReg.test(phoneNumber)) {
-    alert('请填写正确的手机号')
+  if(!relName || !speciality || !className || !qqNumber || !qqNumber || !phoneNumber) {
+    alert('必填信息还未填写完成，请点击确定继续填写')
     return
   }
   if(!qqReg.test(qqNumber)) {
     alert('请填写正确的qq号')
+    return
+  }
+  if(!phoneReg.test(phoneNumber)) {
+    alert('请填写正确的手机号')
     return
   }
   sessionStorage.setItem('qqNumber', JSON.stringify(qqNumber))
